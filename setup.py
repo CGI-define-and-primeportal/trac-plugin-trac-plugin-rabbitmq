f79dc26f14824830e0d9127c468e99cd20659874
Dan Paris <dan.paris@cgi.com>
2015-10-07 12:25:40 +0000

    Adding quick fix for import proton error. Proton should be added to trunk. Refs #5414
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@25892 484df14f-75d2-4b3f-8ba0-490e05069a01

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ setup(
     entry_points = {
         'trac.plugins': [
             'tracfeedrabbitmq = tracfeedrabbitmq',
-            'tracfeedrabbitmq.listeners = tracfeedrabbitmq.listeners',
+            #'tracfeedrabbitmq.listeners = tracfeedrabbitmq.listeners',
             'tracfeedrabbitmq.slack = tracfeedrabbitmq.slack',
-            'tracfeedrabbitmq.servicebus = tracfeedrabbitmq.servicebus',
+            #'tracfeedrabbitmq.servicebus = tracfeedrabbitmq.servicebus',
         ],
     },
 )
